9726e524b08e561e48f3d25e7d1c7f39fb3bd9da
Mark Lasaridi <marklasaridi@gmail.com>
2024-06-04 19:19:52 +0700

    .

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pornhub_api.backends.aiohttp import AioHttpBackend
 app = Flask(__name__)
 
 
-@app.route("/videos")
+@app.route("/ph/videos")
 async def execute():
     async with AioHttpBackend() as backend:
         api = PornhubApi(backend=backend)
@@ -22,9 +22,24 @@ async def execute():
         for vid in videos:
             result.append(
                 {
-                    "name": vid.title,
-                    "categories": [x.category for x in vid.categories],
+                    # "name": vid.title,
+                    # "categories": [x.category for x in vid.categories],
+                    # "thumb": vid.thumb,
+                    "title": vid.title,
+                    "duration": vid.duration,
+                    "views": vid.views,
+                    "video_id": vid.video_id,
+                    "rating": vid.rating,
+                    "ratings": vid.ratings,
+                    "url": vid.url,
+                    "default_thumb": vid.default_thumb,
                     "thumb": vid.thumb,
+                    "publish_date": vid.publish_date,
+                    "segment": vid.segment,
+                    "thumbs": [th.src for th in vid.thumbs],
+                    "tags": [t.tag_name for t in vid.tags],
+                    "categories": [c.category for c in vid.categories],
+                    "pornstars": [p.pornstar_name for p in vid.pornstars],
                 }
             )
 
